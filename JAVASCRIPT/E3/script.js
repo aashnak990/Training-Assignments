fe856b86dfc23211ef9c0516ddcffe79e50aca39
Aashna Kapoor <67586621+aashnak990@users.noreply.github.com>
2020-11-10 13:07:57 +0530

    Add files via upload

---
+++ JAVASCRIPT/E3/script.js
@@ -0,0 +1,12 @@
+var num=5;
+var number= Number(prompt("Guess the Number"));
+if(number<num){
+alert("guessed number is too low")
+}
+
+else if(number===num){
+    alert("your guess is right. woahhh!")
+}
+else if(number>num){
+alert("gussed number is too")
+}
